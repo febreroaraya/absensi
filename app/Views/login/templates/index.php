1e863725ed3af240f8a41cc48562ff118a0fea16
febreroaraya <55268432+febreroaraya@users.noreply.github.com>
2021-10-27 14:23:08 +0700

    a

--- app/Views/login/templates/index.php
+++ app/Views/login/templates/index.php
@@ -12,13 +12,13 @@
     <title>SB - Login Guru</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url(); ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url(); ?>/css/sb-admin-2.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= base_url(); ?>/css/toastr.min.css">
+    <link href="/css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/toastr.min.css">
 
 </head>
 
@@ -27,16 +27,16 @@
 <?= $this->renderSection('content'); ?>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url(); ?>/vendor/jquery/jquery.min.js"></script>
-    <script type="text/javascript" src="<?= base_url(); ?>/js/jquery.validate.min.js"></script>
-    <script type="text/javascript" src="<?= base_url(); ?>/js/toastr.min.js"></script>
-    <script src="<?= base_url(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="/js/toastr.min.js"></script>
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_url(); ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?= base_url(); ?>/js/sb-admin-2.min.js"></script>
+    <script src="/js/sb-admin-2.min.js"></script>
     <script type="text/javascript">
     $("#form_login").validate({
         submitHandler : function(){
